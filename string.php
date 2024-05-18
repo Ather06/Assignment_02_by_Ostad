adda00660281358169c9dc65c52ec95e3a568d1c
Ather06 <mdatherbiswas06@gmail.com>
2024-05-18 22:09:00 +0600

    string.php revised

--- string.php
+++ string.php
@@ -1,21 +1,23 @@
 <?php
+// string array
 $strings = ["Hello", "World", "PHP", "Programming"];
-function countVowels($string){
+function countVowels($string)
+    {
     $vowels = ["a","e","i","o","u", "A", "E", "I", "O", "U"];
     $count = 0;
-    foreach(str_split($string) as $character){
-        if(in_array($character, $vowels)){
+    foreach(str_split($string) as $character)
+    {
+        if(in_array($character, $vowels))
+        {
             $count = $count+1;
         }
     }
     return $count;
-}
-foreach($strings as $string){
+    }
+    foreach($strings as $string)
+    {
     $countVowel = countVowels($string);
     $reversedString = strrev($string);
     echo "Original String: " . $string . "," ." Vowel Count: " . $countVowel . "," . " Reversed String: " . $reversedString."<br>";
-}
-
-
-
+    }
 ?>
