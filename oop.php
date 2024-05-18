843be9ced8e650edaba3d380241067975fd3af43
Ather06 <mdatherbiswas06@gmail.com>
2024-05-18 20:09:16 +0600

    book class added

--- oop.php
+++ oop.php
@@ -1,5 +1,48 @@
 <?php
+class Book{
+    // Add properties as Private
+    private $title;
+    private $availableCopies;
 
+    public function __construct($title, $availableCopies)
+    {
+        // Initialize properties
+        $this -> title = $title;
+        $this -> availableCopies = $availableCopies;
+    }
+
+    // Add getTitle method
+    public function getTitle()
+    {
+        return $this -> title;
+    }
+
+    // Add getAvailableCopies method
+    public function getAvailableCopies()
+    {
+        return $this -> availableCopies;
+    }
+
+    // Add borrowBook method
+    public function borrowBook(){
+        if($this -> availableCopies > 0)
+        {
+            $this -> availableCopies--;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Add returnBook method
+    public function returnBook()
+    {
+        $this -> availableCopies++;
+        return true; 
+    }
+}
 
 
 
